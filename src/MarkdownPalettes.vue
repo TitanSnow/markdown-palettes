74737e0da01021f5aa96ff7ef1383e46e7237bd5
TitanSnow <sweeto@live.cn>
2018-07-17 18:30:50 +0800

    add keyboard shortcuts for some toolbar buttons

--- src/MarkdownPalettes.vue
+++ src/MarkdownPalettes.vue
@@ -11,14 +11,8 @@
                     :class="{'mp-divider':item.name === '|'}"
                     :key="item.name + index">
                     <span v-if="item.name === '|'">|</span>
-                    <a
-                        v-else
-                        :title="t(ensureValue(item.title))"
-                        unselectable="on"
-                        @click="toolbarAction(item)">
-                        <i
-                            :class="['fa', ensureValue(item.icon)]"
-                            unselectable="on">{{ ensureValue(item.content) }}</i>
+                    <a v-else :title="t(ensureValue(item.title)) + (ensureValue(item.keyBinding) ? ` (${ensureValue(item.keyBinding)})`: '')" @click="toolbarAction(item)" unselectable="on">
+                        <i :class="['fa', ensureValue(item.icon)]" unselectable="on">{{ ensureValue(item.content) }}</i>
                     </a>
                 </li>
             </ul>
