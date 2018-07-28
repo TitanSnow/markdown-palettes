60a6683514510e23d146c3de0ae0896efb310b07
TitanSnow <sweeto@live.cn>
2018-07-28 11:15:27 +0800

    init electron

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 module.exports = {
-    pages: {
-        index: {
-            entry: 'src/main.js',
-            template: 'index.html',
-            filename: 'index.html'
-        }
-    },
     configureWebpack: {
         output: {
             globalObject: `(typeof self !== 'undefined' ? self : this)`,
