e20f286d0d9f5d21ab3b8a5ed974dc10a6c284cd
TitanSnow <sweeto@live.cn>
2018-08-02 14:19:06 +0800

    auto load commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,6 +1,20 @@
+import { Command } from './base.ts'
 import _ from 'lodash'
-import Bold from './bold.ts'
-const commands = [Bold]
+const req = require.context('.', false, /\.[jt]sx?$/)
+function issubclass (sub, par) {
+    if (typeof sub !== 'function') return false
+    let cur = sub
+    while (cur !== null) {
+        if (cur === par) return true
+        cur = Object.getPrototypeOf(cur)
+    }
+    return false
+}
+const commands = req
+    .keys()
+    .map(req)
+    .filter(({ default: exp }) => issubclass(exp, Command))
+    .map(({ default: exp }) => exp)
 export default commands
 export function getCommand (command, context) {
     if (typeof command === 'string') {
